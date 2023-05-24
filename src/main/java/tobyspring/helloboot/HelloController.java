e1f4ca31571f49610a1932089ff40d6038044f30
solbae <sol4854@naver.com>
2023-05-24 18:43:17 +0900

    어노테이션을 활용해 매핑정보 등록, 서블릿 컨테이너 초기화를 스프링 컨테이너초기화 할떄 동시에 하기위해 통합

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,10 +1,13 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
 
     // final은 처음 생성할때 꼭 필요하므로 생성자가 꼭! 있어야 합니다.
@@ -14,6 +17,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name){
         // 컨트롤러의 중요한 역할인 유저의 요청사항을 검증하기
         return helloService.sayHello(Objects.requireNonNull(name));
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -13,6 +13,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -24,38 +26,25 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext(); // 스프링 컨테이너 생성
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                // 서블릿 펙토리 : 서블릿 컨테이너를 만드는 것을 쉽게 도와줍니다.
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                // serverFactory.getWebServer : 서블릿 컨테이너 생성하는 메소드입니다.
+                // 따라서 webServer 가 서블릿 컨테이너 입니다.
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this) // DispatcherServlet이 작업을 위임할 applicationContext를 등록
+                    ).addMapping("/*"); // 모든 요청을 다 받는다.
+                });
+                webServer.start();
+            }
+        }; // 스프링 컨테이너 생성
         applicationContext.registerBean(HelloController.class); // 메타정보를 넣어 Bean을 생성
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh(); // Bean을 만드는 명령어
-
-        // 서블릿 펙토리 : 서블릿 컨테이너를 만드는 것을 쉽게 도와줍니다.
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        // serverFactory.getWebServer : 서블릿 컨테이너 생성하는 메소드입니다.
-        // 따라서 webServer 가 서블릿 컨테이너 입니다.
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통기능 처리
-                    // url 요청 처리
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        // 컨트롤러 클래스를 사용하기
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name); // 생략
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE); // 타입
-                        resp.getWriter().print(ret); // 바디
-                    }
-                    else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*"); // 모든 요청을 다 받는다.
-        });
-        webServer.start();
     }
 
 }

3205040dacace802b9236aaff2cc5c76c856f093
solbae <sol4854@naver.com>
2023-05-24 17:42:51 +0900

    컨트롤러의 역할만 하기위해 service 생성 후 역할 위임

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,8 +3,12 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name){
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+        // 컨트롤러의 중요한 역할인 유저의 요청사항을 검증하기
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }

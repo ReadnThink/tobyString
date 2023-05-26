ea5ca319475a4b4013c6f325bea9db452db7b6f8
solbae <sol4854@naver.com>
2023-05-26 23:26:40 +0900

    데코레이터 패턴 추가

---
+++ src/main/java/tobyspring/helloboot/HelloDecorator.java
@@ -0,0 +1,19 @@
+package tobyspring.helloboot;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary // Bean 후보가 2개만 있을때 우선적으로 DI가 됩니다.
+public class HelloDecorator implements HelloService{
+    private final HelloService helloService;
+
+    public HelloDecorator(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @Override
+    public String sayHello(String name) {
+        return "*" + helloService.sayHello(name) + "*";
+    }
+}

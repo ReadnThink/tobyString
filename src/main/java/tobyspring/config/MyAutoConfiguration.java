925e3d53e90ab0f842d050c9b70d89d889c320a8
solbae <sol4854@naver.com>
2023-05-30 17:23:58 +0900

    자동구성 애노테이션 적용

--- src/main/java/tobyspring/config/MyAutoConfiguration.java
+++ src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -7,8 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 자동구성정보 설정을 하는 클래스를 알리기 위함
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration {
 }

6fe0a70433aaf4eb88df18ff3dc6825ae198fa98
solbae <sol4854@naver.com>
2023-06-01 17:38:08 +0900

    @Value를 사용하여 프로퍼티 주입

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -13,11 +14,13 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")// application.properties에서 읽어옵니다.
+    String contextPath;
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 같은 타입의 Bean이 없다면 생성해라
-    public ServletWebServerFactory servletWebServerFactory(Environment env){
+    public ServletWebServerFactory servletWebServerFactory(){
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath")); // application.properties에서 읽어옵니다.
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }

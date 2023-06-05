ee4003f2ef7817977f350c54c03cc14404ef39b0
solbae <sol4854@naver.com>
2023-06-05 17:52:25 +0900

    JdbcTempate, 트랜잭션 매니저 설정 및 테스트

--- src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -2,8 +2,12 @@ package tobyspring.config.autoconfig;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
@@ -13,6 +17,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement // 트랜잭션 매니져를 적용하여 @Transaction 어노테이션을 사용 할 수 있게 합니다.
 public class DataSourceConfig {
 
     @Bean
@@ -42,5 +47,19 @@ public class DataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // DataSource Bean 이 한개만 등록되었다면 사용한다는 뜻.
+    @ConditionalOnMissingBean // 커스텀으로 등록할 것을 대비
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // DataSource Bean 이 한개만 등록되었다면 사용한다는 뜻.
+    @ConditionalOnMissingBean // 커스텀으로 등록할 것을 대비
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 }
